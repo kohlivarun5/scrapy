b68be222e8ef108dc39631900a455dbe41bb1096
Varun Kohli <kohli_varun5@yahoo.com>
2016-02-04 17:01:37 -0500

    finalize

--- vault_150_under_150/under_150.py
+++ vault_150_under_150/under_150.py
@@ -1,12 +1,15 @@
 import scrapy
+import urlparse
 
 class FirmItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     location = scrapy.Field()
-    website = scrapy.Field()
+    page = scrapy.Field()
+    url = scrapy.Field()
+    size = scrapy.Field()
 
-class StackOverflowSpider(scrapy.Spider):
+class VaultSpider(scrapy.Spider):
     name = 'vault'
     allowed_domains = ["vault.com"]
     start_urls = [
@@ -25,26 +28,34 @@ class StackOverflowSpider(scrapy.Spider):
           continue
 
         item = FirmItem()
-        item["name"] = div.xpath('./td/a/text()').extract()[0]
-        item["location"] = div.xpath('./td/text()').extract()[0]
+        item["name"] = div.xpath('td[1]/a/text()').extract()[0]
+        item["location"] = div.xpath('td[2]/text()').extract()[0].strip()
+
+        full_url = urlparse.urljoin(response.url, div.xpath('td[1]/a/@href').extract()[0])
+        item['page'] = full_url
+        
+        items.append(item)
+
+        yield scrapy.Request(full_url, callback=(lambda response,val=item: self.parse_firm_page(response, val)))
+
+
+
+    def parse_firm_page(self, response,item):
+
+        try:
+            item['url'] = response.xpath('//div[@id="ContentPlaceHolderDefault_SectionContent_EntityTabInfo_RightDivInfo"]/div[2]/p/a/@href').extract()[0]
+
+            for stats in response.xpath('//div[@id="ContentPlaceHolderDefault_SectionContent_EntityTabInfo_RightDivInfo"]/ul/li'):
+                
+                stat_text = stats.xpath('text()').extract()[0]
+
+                if 'Total' in stat_text:
+                    item['size'] = stat_text.split()[-1]
+        except:
+            pass
+
+
         yield item
-
-
-#    start_urls = ['http://stackoverflow.com/questions?sort=votes']
-#
-#    def parse(self, response):
-#        for href in response.css('.question-summary h3 a::attr(href)'):
-#            full_url = response.urljoin(href.extract())
-#            yield scrapy.Request(full_url, callback=self.parse_question)
-#
-#    #def parse_question(self, response):
-#    #    yield {
-#    #        'title': response.css('h1 a::text').extract()[0],
-#    #        'votes': response.css('.question .vote-count-post::text').extract()[0],
-#    #        'body': response.css('.question .post-text').extract()[0],
-#    #        'tags': response.css('.question .post-tag::text').extract(),
-#    #        'link': response.url,
-#    #    }
 
 
 #import scrapy
